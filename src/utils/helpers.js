11b464a9f6a9d5ff98e794517f4c9a51d2ef38ec
Spartacus <k_morter@outlook.com>
2020-09-11 09:59:49 -0600

    added initial react app files

---
+++ src/utils/helpers.js
@@ -0,0 +1,10 @@
+//capitalize first letter function
+export function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+//validate email in contact form function
+export function validateEmail(email) {
+    var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
