bdf4285349bb9740e429732d0794378ead3d0401
Spartacus <k_morter@outlook.com>
2020-09-11 15:15:22 -0600

    styled portfolio cards

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ProjectCard from '../ProjectCard'
 import projects from '../../projects.json'
 
+
 function Wrapper(props) {
     return <div className="wrapper">{props.children}</div>;
 }
@@ -11,12 +12,12 @@ function Portfolio() {
     return (
         <div>
             <div>
-                <h1 className="text-center">Web Development Portfolio</h1>
+                <h1 className="text-center portfolio-title">Web Development Portfolio</h1>
             </div>
 
-            <Wrapper>
+            <Wrapper >
                 {projects.map((project) => (
-                    <ProjectCard  key={project.id} name={project.name} image={project.image} github={project.github} deploy={project.deploy}/>
+                    <ProjectCard key={project.id} name={project.name} image={project.image} github={project.github} deploy={project.deploy}/>
                 ))}
             </Wrapper>
         </div>
--- src/components/ProjectCard/index.js
+++ src/components/ProjectCard/index.js
@@ -7,8 +7,8 @@ function ProjectCard(props) {
             <div className="img-container">
                 <img alt={props.name} src={props.image} />
             </div>
-            <div className="content">
-                <p className="project-title">{props.name}</p>
+            <div className="portfolio-content">
+                <p>{props.name}</p>
                 <p>
                     <a href={props.github}><img src="https://img.icons8.com/color/48/000000/github--v1.png" alt="Repository" id="project-icon"/></a> 
                     <a href={props.deploy}><img src="https://img.icons8.com/color/48/000000/monitor.png" alt="Deployed Site" id="project-icon"/></a> 
