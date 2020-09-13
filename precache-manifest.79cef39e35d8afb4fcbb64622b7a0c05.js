961ada2b2fe341a937dee67f2237aa3f9f433a19
Spartacus <k_morter@outlook.com>
2020-09-13 11:00:51 -0600

    Updates

---
+++ precache-manifest.79cef39e35d8afb4fcbb64622b7a0c05.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f73f1b887e45a588e502ab9f181b7d07",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "5c6aa2817bfdb389c7ba",
+    "url": "/react-portfolio/static/css/main.2e53b5ca.chunk.css"
+  },
+  {
+    "revision": "7e0649ed48d66ff95571",
+    "url": "/react-portfolio/static/js/2.51d6072f.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-portfolio/static/js/2.51d6072f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c6aa2817bfdb389c7ba",
+    "url": "/react-portfolio/static/js/main.ac29672e.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  },
+  {
+    "revision": "d23d5a4f40c5929311d0ecc0b361a985",
+    "url": "/react-portfolio/static/media/kailey_portrait.d23d5a4f.jpg"
+  }
+]);
